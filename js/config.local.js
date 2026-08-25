51654468920a6730058c75d588ce1f659058bd0b
Ioannidis Ioannis <109786525+iioannidis94@users.noreply.github.com>
2026-08-25 12:26:41 +0300

    Update config.local.js

--- js/config.local.js
+++ js/config.local.js
@@ -1,2 +1,5 @@
 // js/config.local.js  ← ΔΕΝ ανεβαίνει στο GitHub
-const TMDB_KEY = 'e57a4ef6bc85d0408c926e152f3269bb'; 
+
+const k1 = "e57a4ef6bc85d040"
+const k2 = "8c926e152f3269bb"
+const TMDB_KEY = k1 + k2; 
